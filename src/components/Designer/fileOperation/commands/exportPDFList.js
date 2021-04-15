1f8ff2a903c05918338fa2b4db815e174183147f
crazyman106 <291924028@qq.com>
2021-04-15 10:07:03 +0800

    收工

--- src/components/Designer/fileOperation/commands/exportPDFList.js
+++
@@ -1,9 +0,0 @@
-
-var ExportPDFList = {
-    title: "PDF_List",
-    type: "dropdown",
-    commandName: "ExportPDFList",
-    subCommands: ["ExportWorksheetPDF"]
-}
-
-export default ExportPDFList;
